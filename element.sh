6ac5dc11ca4bb41b54651e8524dd363be08855e1
Nelson Alesandro <131645782+naorella@users.noreply.github.com>
2024-06-24 01:33:01 -0400

    feat: GET_INFO to element.sh
    Added GET_INFO to element.sh so that the case of not being able
    to find an element resulted in the proper output. If the
    database seach returned an empty result the script will say
    as much. And to avoid having multiple instances of calling
    GET_INFO for each case, it was moved to the end of the if cases
    and wont return anything if given an empty result, in the case
    of no database matches.

--- element.sh
+++ element.sh
@@ -1,6 +1,32 @@
 #! /bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
+#the function that will retrieve and show the information
+GET_INFO(){
+  if [[ $1 ]]
+    #if the argument we get is not empty
+  then
+    #rename variable for better readability
+    ATOM_NUM=$1
+    #get info from the properties table
+    PROP_INFO=$($PSQL "SELECT * FROM properties WHERE atomic_number = $ATOM_NUM")
+    #get info from the elements table
+    ELEM_INFO=$($PSQL "SELECT * FROM elements WHERE atomic_number = $ATOM_NUM")
+
+    #read both results and assign each column result a variable name
+    read -r ELEM_NUM BAR ELEM_SYM BAR ELEM_NAME <<< $(echo "$ELEM_INFO")
+    read -r ELEM_NUM BAR ELEM_WEIGHT BAR MELT BAR BOIL BAR TYPE_ID <<< $(echo "$PROP_INFO")
+    
+    #using TYPE_ID find the type on the types table
+    TYPE_INFO=$($PSQL "SELECT * FROM types WHERE type_id = $TYPE_ID")
+    #read the result to assign variables
+    read -r TYPE_ID BAR ELEM_TYPE <<< $(echo "$TYPE_INFO")
+
+    #combine all the info we have and format it into a sentence
+    echo "The element with atomic number $ATOM_NUM is $ELEM_NAME ($ELEM_SYM). It's a $ELEM_TYPE, with a mass of $ELEM_WEIGHT amu. $ELEM_NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
+    fi
+}
+
 MAIN(){
   if [[ $1 ]]
   #if argument provided
@@ -22,17 +48,12 @@ MAIN(){
         then
           #say we couldn't find it
           echo "I could not find that element in the database."
-        else
-          ATOM_NUM=$RETURN_RESULT
-          PROP_INFO=$($PSQL "SELECT * FROM properties WHERE atomic_number = $ATOM_NUM")
-          ELEM_INFO=$($PSQL "SELECT * FROM elements WHERE atomic_number = $ATOM_NUM")
-          read -r ELEM_NUM BAR ELEM_SYM BAR ELEM_NAME <<< $(echo "$ELEM_INFO")
-          read -r ELEM_NUM BAR ELEM_TYPE BAR ELEM_WEIGHT BAR BOIL BAR MELT TYPE_ID <<< $(echo "$PROP_INFO")
-          echo "$ELEM_NUM $ELEM_SYM $ELEM_NAME"
-          
         fi
       fi
     fi
+    #GET_INFO at end so we don't have to call it three times after each case
+    #If nothing was found it will pass an empty variable
+    GET_INFO $RETURN_RESULT
   else
     echo "Please provide an element as an argument."
   fi
