d9762347aacab4b51c0939844b560addd4638088
Nelson Alesandro <131645782+naorella@users.noreply.github.com>
2024-06-24 00:39:42 -0400

    fix: element.sh now returns proper message for invalid input
    element.sh has had the if elif structure rearanged so that
    when an invalid input is entered, the script responds
    with the correct respons and terminates, instead
    of continuing with the base case.

--- element.sh
+++ element.sh
@@ -17,16 +17,22 @@ MAIN(){
       then
         #check to see if its a name
         RETURN_RESULT=$($PSQL "SELECT atomic_number FROM elements WHERE '$1' = name")
+        if [[ -z $RETURN_RESULT ]]
+          #if still nothing
+        then
+          #say we couldn't find it
+          echo "I could not find that element in the database."
+        else
+          ATOM_NUM=$RETURN_RESULT
+          PROP_INFO=$($PSQL "SELECT * FROM properties WHERE atomic_number = $ATOM_NUM")
+          ELEM_INFO=$($PSQL "SELECT * FROM elements WHERE atomic_number = $ATOM_NUM")
+          read -r ELEM_NUM BAR ELEM_SYM BAR ELEM_NAME <<< $(echo "$ELEM_INFO")
+          read -r ELEM_NUM BAR ELEM_TYPE BAR ELEM_WEIGHT BAR BOIL BAR MELT TYPE_ID <<< $(echo "$PROP_INFO")
+          echo "$ELEM_NUM $ELEM_SYM $ELEM_NAME"
+          
+        fi
       fi
     fi
-    ATOM_NUM=$RETURN_RESULT
-    PROP_INFO=$($PSQL "SELECT * FROM properties WHERE atomic_number = $ATOM_NUM")
-    ELEM_INFO=$($PSQL "SELECT * FROM elements WHERE atomic_number = $ATOM_NUM")
-    read -r ELEM_NUM BAR ELEM_SYM BAR ELEM_NAME <<< $(echo "$ELEM_INFO")
-    read -r ELEM_NUM BAR ELEM_TYPE BAR ELEM_WEIGHT BAR BOIL BAR MELT TYPE_ID <<< $(echo "$PROP_INFO")
-
-    echo "$ELEM_NUM $ELEM_SYM $ELEM_NAME"
-
   else
     echo "Please provide an element as an argument."
   fi
