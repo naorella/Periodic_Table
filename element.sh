5c4411b5150fcd0c0140fe7d75b8128cdd537c0a
Nelson Alesandro <131645782+naorella@users.noreply.github.com>
2024-06-24 00:27:57 -0400

    feat: element.sh to retrieve all relevent info
    element.sh now retrieves relevent info about the element provided,
    given an atomic number, symbol, or name. The script returns the
    psql result and pipes it into the read command to splice it into
    variables.

--- element.sh
+++ element.sh
@@ -19,9 +19,17 @@ MAIN(){
         RETURN_RESULT=$($PSQL "SELECT atomic_number FROM elements WHERE '$1' = name")
       fi
     fi
+    ATOM_NUM=$RETURN_RESULT
+    PROP_INFO=$($PSQL "SELECT * FROM properties WHERE atomic_number = $ATOM_NUM")
+    ELEM_INFO=$($PSQL "SELECT * FROM elements WHERE atomic_number = $ATOM_NUM")
+    read -r ELEM_NUM BAR ELEM_SYM BAR ELEM_NAME <<< $(echo "$ELEM_INFO")
+    read -r ELEM_NUM BAR ELEM_TYPE BAR ELEM_WEIGHT BAR BOIL BAR MELT TYPE_ID <<< $(echo "$PROP_INFO")
+
+    echo "$ELEM_NUM $ELEM_SYM $ELEM_NAME"
+
   else
     echo "Please provide an element as an argument."
   fi
 }
 
-MAIN
+MAIN $1
