d5152ea1055ae30768712a8ef4e5b858ae45f54f
Nelson Alesandro <131645782+naorella@users.noreply.github.com>
2024-06-23 20:57:02 -0400

    feat: 'if' cases to element.sh
    Added the three cases to MAIN() to check to see what data type was entered

--- element.sh
+++ element.sh
@@ -3,8 +3,22 @@ PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
 MAIN(){
   if [[ $1 ]]
+  #if argument provided
   then
+    #check to see if its an atomic number
     RETURN_RESULT=$($PSQL "SELECT atomic_number FROM elements WHERE $1 = atomic_number")
+    if [[ -z $RETURN_RESULT ]]
+    #if not then
+    then
+      #check to see if its a symbol
+      RETURN_RESULT=$($PSQL "SELECT atomic_number FROM elements WHERE '$1' = symbol")
+      if [[ -z $RETURN_RESULT ]]
+      #if not then
+      then
+        #check to see if its a name
+        RETURN_RESULT=$($PSQL "SELECT atomic_number FROM elements WHERE '$1' = name")
+      fi
+    fi
   else
     echo "Please provide an element as an argument."
   fi
